c4e106081ccf3e03762e16b7c77d57d97c7c23bd
chrisahart <chrissahart@gmail.com>
2024-11-06 20:03:24 +0800

    Working on hafnia junction

--- python/pymatgen_build_hfo2_cu_capacitor.py
+++ python/pymatgen_build_hfo2_cu_capacitor.py
@@ -26,8 +26,8 @@ junction = Atoms(pbc=True)
 cu_max = np.max(cu_slab.positions[:, 2])
 
 for atom in cu_slab:
-    # atom.position[0] = atom.position[0] + 1.27500
-    # atom.position[1] = atom.position[1] + (2.60000 - 1.27633)
+    atom.position[0] = atom.position[0] + 1.27500
+    atom.position[1] = atom.position[1] + (2.60000 - 1.27633)
     junction.append(atom)
 
 for atom in hafnia_supercell:
@@ -60,33 +60,41 @@ junction = junction[sorted_indices]
 # Label each according to z index
 
 # Remove Hf and O to form capacitor
-mask = np.isin(junction.symbols, ['Hf', 'O'], invert=True)
-junction = Atoms(symbols=junction.symbols[mask],
-                 positions=junction.positions[mask])
+# mask = np.isin(junction.symbols, ['Hf', 'O'], invert=True)
+# junction = Atoms(symbols=junction.symbols[mask],
+#                  positions=junction.positions[mask])
 
 # Round all atom positions to 3 dp
 for atom in junction:
     atom.position = np.round(atom.position, 3)
 
 # Round all atom positions to 3 dp
-unique_z = np.unique(junction.positions[:, 2])
-species = []
-for i in range(unique_z.shape[0]):
-    for atom in junction:
-        if atom.position[2] == unique_z[0]:
-            species.append('Cu_{}'.format(str(i+1)))
+# unique_z = np.unique(junction.positions[:, 2])
+# species = []
+# j = 1
+# for i in range(unique_z.shape[0]):
+#     for atom in junction:
+#         if atom.symbol == 'Cu':
+#             if atom.position[2] == unique_z[0]:
+#                 species.append('Cu_{}'.format(str(j)))
+#             j=j+1
+#
+
+# Need copy symbols from atoms object and then change species au to au_1 etc.
+# Clean up file and
 
 # Convert to Pandas dataframe so we can print custom species
-df1 = pd.DataFrame({"X":junction.positions[:, 0],
-                    "Y":junction.positions[:, 1],
-                    "Z":junction.positions[:, 2]})
-df1.insert(loc=0, column='Species', value=pd.Series(species).values)
-print(df1)
-num_atoms = 336
-filename_output = "{}/{}_junction_capacitor_10layers.xyz".format(folder, polymorph)
-print_xyz.print_from_pandas(df1, num_atoms, filename_output, save_dp='%.8f')
+# df1 = pd.DataFrame({"X":junction.positions[:, 0],
+#                     "Y":junction.positions[:, 1],
+#                     "Z":junction.positions[:, 2]})
+# df1.insert(loc=0, column='Species', value=pd.Series(species).values)
+# print(df1)
+# num_atoms = len(species)
+# filename_output = "{}/{}_junction_10layers.xyz".format(folder, polymorph)
+# # filename_output = "{}/{}_junction_capacitor_10layers.xyz".format(folder, polymorph)
+# print_xyz.print_from_pandas(df1, num_atoms, filename_output, save_dp='%.8f')
 
-# write("{}/{}_junction.xyz".format(folder, polymorph), junction)
+write("{}/{}_junction.xyz".format(folder, polymorph), junction)
 # write("{}/{}_junction_capacitor_10layers.xyz".format(folder, polymorph), junction)
 view(junction)
 
